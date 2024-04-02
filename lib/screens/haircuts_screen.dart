e3440d8bfae04a9320d45172a6c3d3a632f50dae
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2024-04-01 22:07:13 -0300

    added haircut screen

---
+++ lib/screens/haircuts_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HaircutsScreen extends StatelessWidget {
+  const HaircutsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
