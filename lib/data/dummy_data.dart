969c3c66020bde65365c4f36b34b6b426c9ad13e
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2024-04-01 22:34:58 -0300

    added dummy data

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,46 @@
+import '../models/haircut.dart';
+
+final List<Haircut> dummyData = [
+  Haircut(
+    description: 'Degradê masculino',
+    price: 30.0,
+    imageUrl:
+        'https://static.wixstatic.com/media/aff363_e32659d7e77a450cb9dc3b27cc62c4fb~mv2.png/v1/fit/w_592%2Ch_678%2Cal_c%2Cq_80,enc_auto/file.jpg',
+  ),
+  Haircut(
+    description: 'Degradê com sobrancelha',
+    price: 35.0,
+    imageUrl:
+        'https://i.pinimg.com/236x/57/45/70/574570f424b6a687302fac9158746a43.jpg',
+  ),
+  Haircut(
+    description: 'Luzes masculinas',
+    price: 40.0,
+    imageUrl:
+        'https://st4allthings4p4ci.blob.core.windows.net/allthingshair/allthingshair/wp-content/uploads/sites/2/2016/06/10155729/luzes-reflexo-alinhado-cabelo-masculino-cacheado-curto.jpg',
+  ),
+  Haircut(
+    description: 'Corte social masculino',
+    price: 25.0,
+    imageUrl:
+        'https://belezadohomem.com.br/wp-content/uploads/2023/08/corte-social-masculino07.jpg',
+  ),
+  Haircut(
+    description: 'Calvo de Cria',
+    price: 20.0,
+    imageUrl:
+        'https://www.opovo.com.br/_midias/jpg/2022/07/05/entradas_cria_barbearia_rio_grande_do_su_tres_passos-19135662.jpg',
+  ),
+  Haircut(
+    description: 'Corte do Ratão Pernambucano',
+    price: 20.0,
+    imageUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiqV92XDeDlXk5Yk51f2_-Zqzc3ebVZ07IweOXFvQtZg&s',
+  ),
+  Haircut(
+    description: 'Corte do Luva de Pedreiro RECEBAAA!!!',
+    price: 30.0,
+    imageUrl:
+        'https://conteudo.imguol.com.br/c/esporte/bb/2022/04/07/iran-ferreira-o-cara-da-luva-de-pedreiro-em-quijingue-na-bahia-1649366478082_v2_3x4.jpg',
+  ),
+];
