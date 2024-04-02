91e224763199e3abe76adf8bd9ad5477329222fd
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2024-04-01 22:35:10 -0300

    added image url attr

--- lib/models/haircut.dart
+++ lib/models/haircut.dart
@@ -1,9 +1,11 @@
 class Haircut {
   final String description;
   final double price;
+  final String imageUrl;
 
   Haircut({
     required this.description,
     required this.price,
+    required this.imageUrl,
   });
 }
