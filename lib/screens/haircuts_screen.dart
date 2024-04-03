953f7087a9614cfcdc49106cda8ea9e4c94b1559
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2024-04-03 12:07:18 -0300

    changed listview to gridview

--- lib/screens/haircuts_screen.dart
+++ lib/screens/haircuts_screen.dart
@@ -1,17 +1,33 @@
 import 'package:barber_shop_sm/data/dummy_data.dart';
+import 'package:barber_shop_sm/models/order.dart';
 import 'package:barber_shop_sm/widgets/haircut_item.dart';
 import 'package:flutter/material.dart';
 
 class HaircutsScreen extends StatelessWidget {
-  const HaircutsScreen({super.key});
+  final Function(Order) onAddOrder;
+
+  const HaircutsScreen({
+    super.key,
+    required this.onAddOrder,
+  });
 
   @override
   Widget build(BuildContext context) {
     var haircuts = dummyData;
 
-    return ListView.builder(
+    return GridView.builder(
       itemCount: haircuts.length,
-      itemBuilder: (ctx, i) => HaircutItem(haircut: haircuts[i]),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 20,
+        childAspectRatio: 3 / 2,
+        mainAxisExtent: 310,
+      ),
+      itemBuilder: (ctx, i) => HaircutItem(
+        haircut: haircuts[i],
+        onAddOrder: onAddOrder,
+      ),
     );
   }
 }
