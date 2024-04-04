e08702c0b945f1265077c369735f8a5915ed8ad5
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2024-04-03 21:51:33 -0300

    added id attr

--- lib/models/order.dart
+++ lib/models/order.dart
@@ -1,9 +1,15 @@
 import 'package:barber_shop_sm/models/haircut.dart';
 
 class Order {
+  final String id;
   final Haircut haircut;
   final DateTime time;
   final double total;
 
-  Order({required this.haircut, required this.time, required this.total});
+  Order({
+    required this.id,
+    required this.haircut,
+    required this.time,
+    required this.total,
+  });
 }
