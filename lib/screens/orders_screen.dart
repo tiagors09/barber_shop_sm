baaac39f262ebd65af71f234e31f46aac097f71e
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2024-04-03 22:07:46 -0300

    added key to each order, formated how to show orders

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -2,7 +2,7 @@ import 'package:barber_shop_sm/models/order.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   final List<Order> orders;
   final Function(int) onRemoveOrder;
 
@@ -13,26 +13,45 @@ class OrdersScreen extends StatelessWidget {
   });
 
   @override
+  State<OrdersScreen> createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: orders.length,
+      itemCount: widget.orders.length,
       itemBuilder: (ctx, i) => Card(
+        key: GlobalObjectKey(widget.orders[i]),
         margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
         child: ListTile(
-          title: Text(orders[i].haircut.description),
-          subtitle: Text(DateFormat('dd MM yy').format(orders[i].time)),
+          title: Text(widget.orders[i].haircut.description),
+          subtitle: Text(DateFormat('dd MM yy').format(widget.orders[i].time)),
           leading: CircleAvatar(
             child: Text(
-              'R\$ ${orders[i].haircut.price}',
+              'R\$ ${widget.orders[i].haircut.price}',
               style: const TextStyle(fontSize: 11),
             ),
           ),
-          trailing: IconButton(
-            onPressed: () => onRemoveOrder(i),
-            icon: const Icon(
-              Icons.delete,
-              color: Colors.red,
-            ),
+          trailing: Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.edit),
+              ),
+              IconButton(
+                onPressed: () {
+                  widget.onRemoveOrder(i);
+                  setState(() {
+                    widget.orders.removeAt(i);
+                  });
+                },
+                icon: const Icon(
+                  Icons.delete,
+                  color: Colors.red,
+                ),
+              ),
+            ],
           ),
         ),
       ),
